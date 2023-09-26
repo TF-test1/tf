4fb6809ce4d908e83b50c14d9bba737a005976b4
Menaga Muthuramalingam <107127252+Menaga12@users.noreply.github.com>
2023-09-26 12:07:10 +0530

    Update cloudsql.tf

--- google_cloud/camunda-secure/cloudsql.tf
+++ google_cloud/camunda-secure/cloudsql.tf
@@ -1,12 +1,1 @@
 
-
-resource "google_sql_user" "user" {
-  name     = "camundasecure"
-  instance = google_sql_database_instance.camunda-db.name
-  password = "futurice"
-}
-
-resource "google_sql_database" "database" {
-  name     = "camundasecure"
-  instance = google_sql_database_instance.camunda-db.name
-}
