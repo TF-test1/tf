00dd6f7d90204fcf7f4162a59535defefe77967c
Menaga Muthuramalingam <107127252+Menaga12@users.noreply.github.com>
2023-11-15 17:50:09 +0530

    Update db.tf

--- aws/wordpress_fargate/db.tf
+++ aws/wordpress_fargate/db.tf
@@ -3,28 +3,7 @@ resource "random_string" "snapshot_suffix" {
   special = false
 }
 
-resource "aws_rds_cluster" "this" {
-  cluster_identifier      = "${var.prefix}-${var.environment}"
-  engine                  = "aurora"
-  engine_mode             = "serverless"
-  vpc_security_group_ids  = [aws_security_group.db.id]
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  engine_version          = var.db_engine_version
-  availability_zones      = data.aws_availability_zones.this.names
-  database_name           = "wordpress"
-  master_username         = var.db_master_username
-  master_password         = var.db_master_password
-  backup_retention_period = var.db_backup_retention_days
-  preferred_backup_window = var.db_backup_window
-  scaling_configuration {
-    auto_pause               = var.db_auto_pause
-    seconds_until_auto_pause = var.db_seconds_until_auto_pause
-    max_capacity             = var.db_max_capacity
-    min_capacity             = var.db_min_capacity
-  }
-  final_snapshot_identifier = "${var.prefix}-${var.environment}-${random_string.snapshot_suffix.result}"
-  tags                      = var.tags
-}
+
 
 resource "aws_db_subnet_group" "this" {
   name       = "${var.prefix}-${var.environment}"
